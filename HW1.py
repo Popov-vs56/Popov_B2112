ec38a7cfddc6e1aa489f7b4e2c260d94218b3ec9
b2112 <viacheslav.martson@gmail.com>
2024-12-08 17:50:56 +0200

    Lesso2

---
+++ HW1.py
@@ -0,0 +1,16 @@
+#1004
+'''
+num = float(input("Enter num:"))
+integer_num = int(num)
+print(round(num))
+print(integer_num)
+print(f"{(num-integer_num):.1f}")
+'''
+#1005
+'''
+x,y = map(int, input().split())
+print(line)
+res = x**3+(x+1)/(y**2-1)
+print(f"{res:.1f}")
+'''
+#1006
---
+++ Lesson2.py
@@ -0,0 +1,27 @@
+#1007
+'''
+N = float(input())
+N = int(N)*2
+print(N)'''
+#1008
+'''
+P, K = map(float,input().split())
+P_float = P - int(P)
+res = round(P_float*K)
+print(res)
+'''
+#1009
+"""
+a1,b1,a2,b2 = map(int,input().split())
+S1 = a1 * b1
+S2 = a2 * b2
+res = abs(S1-S2)
+print(res)
+"""
+
+#1010
+'''
+num = int(input())
+print((num//10)*(num%10))
+'''
+
