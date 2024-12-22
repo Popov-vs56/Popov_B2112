4dd8cf6d3aa29d5dc55268a58f432eb299d4096b
b2112 <viacheslav.martson@gmail.com>
2024-12-22 17:33:34 +0200

    Lesson5

--- Lesson5,6/Lesson5.py
+++ Lesson5,6/Lesson5.py
@@ -4,7 +4,29 @@ lst = [randint(-10, 10) for i in range(20)]
 print(lst)
 
 sum_negative = 0
-
 for num in lst:
     if num < 0:sum_negative += num
-    print("Sum negative item list: ", sum_negative)
+print("Sum negative item list: ", sum_negative)
+
+sum_of_even = 0
+for num in lst:
+    if num % 2 == 0:sum_of_even += num
+print("Sum of even item list: ", sum_of_even)
+
+
+sum_of_odd = 0
+for num in lst:
+    if num % 2 != 0:
+        sum_of_odd += num
+print("Sum of odd item list: ", sum_of_odd)
+
+sum_elements_mult_3 = 0
+for num in lst:
+    if num % 3 == 0:
+        sum_elements_mult_3 += num
+print("Sum of elements items multiply of 3 list: ", sum_elements_mult_3)
+
+min_value_list = min(lst)
+max_value_list = max(lst)
+index_min = lst.index(min_value_list)
+index_max = lst.index(max_value_list)
