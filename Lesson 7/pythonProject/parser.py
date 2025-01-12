5bd4ff9ac1104d9394b1d5e983c605f4e91a958d
b2112 <viacheslav.martson@gmail.com>
2025-01-12 16:23:40 +0200

    lesson 9 parser

--- Lesson 7/pythonProject/parser.py
+++ Lesson 7/pythonProject/parser.py
@@ -1,10 +1,22 @@
 import requests
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
 
 url = "https://uaserials.pro/films/"
+# for i in range(1,10):
+#     url_new = url
+#     url_new+= str(i)
+#     url_new+= "/"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, features="html.parser")
 
+name_list = []
+
 soup_list_name = soup.find_all('div',{'class':'th-title truncate'})
 for i in soup_list_name:
-    print(soup)
+    print(i.text)
+    name_list.append(i.text)
+
+f = open("film.txt", "a")
+for film in name_list:
+    f.write(f"{film}\n")
+f.close()
